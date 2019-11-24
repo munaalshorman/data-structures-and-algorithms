e0d1f7e2ce165b4dd32dd2180b2bc97a555f7398
muna alshorman <munaalshorman123@gmail.com>
2019-11-25 01:16:08 +0200

    code without solving

--- code-challenges/challenge-9/challenges-09.test.js
+++ code-challenges/challenge-9/challenges-09.test.js
@@ -3,13 +3,15 @@
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1
 
-Write a function named countNumberOfElements that, given an array as input, uses reduce to count the number of elements in the array.
+Write a function named countNumberOfElements that, given an array as input, uses reduce to count the number
+ of elements in the array.
 
 Note: You may not use the array's built-in length property.
 ------------------------------------------------------------------------------------------------ */
 
 const countNumberOfElements = (arr) => {
   // Solution code here...
+  
 };
 
 /* ------------------------------------------------------------------------------------------------
